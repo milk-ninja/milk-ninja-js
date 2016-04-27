2b4a01028853230b7ccc024e73b0527a3f9424c9
Mallerie Shirley <mallerie@gmail.com>
2016-04-27 09:51:23 -0400

    notes on how to work on geo location

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { Link } from 'react-router';
+import { Link, hashHistory } from 'react-router';
+
+// import FourSquare from './foursquare';
+
 
 
 
 export default class Dashboard extends Component {
+  constructor(...args) {
+    super(...args);
+    this.state = {
+
+      place: [],
+      value: ''
+    }
+  }
+
+  clickHandler(){
+
+    // FourSquare.get('/place', {q: this.state.value}).then(places => {
+
+    //   this.setState({places});
+
+    // });
+
+  }
+
+  changeHandler(event){
+
+    this.setState({
+
+      value: event.target.value
+
+    });
+
+  }
   
   render() {
     return(
@@ -12,10 +43,9 @@ export default class Dashboard extends Component {
         <div className="title-image-describe-wrapper">
           <h1>Dashboard</h1>
           <img src="./images/babyninja-300x300.png"/>
-          <h2>Find a location.</h2>
-          <input type="text" placeholder="Search"/>
-          <button>Submit</button>
-          
+          <h2>Pinpointing your location...</h2>          
+          <button onClick={::this.clickHandler}>Submit</button>
+
         </div>
         <div className="register-login-wrapper">
           <Link to="/register">Back to Home</Link>
@@ -25,3 +55,11 @@ export default class Dashboard extends Component {
       )
   }
 }
+
+// find the user
+// loading bar
+// ajax request to back end 
+// load the list of places
+
+// later: update the text to show that the page is updating and then that it has been found
+// later: after 5 seconds error that they cant be found, turn on location services
--- src/js/home.js
+++ src/js/home.js
@@ -11,12 +11,12 @@ export default class Home extends Component {
       <div className="home">
         <div className="title-image-describe-wrapper">
           <h1>Milk Ninja</h1>
+          <Link to="/login">Login</Link>
           <img src="./images/babyninja-300x300.png"/>
           <h2>Find great locations where you can comfortably feed your baby.</h2>
         </div>
         <div className="register-login-wrapper">
           <Link to="/register">Register</Link>
-          <Link to="/login">Login</Link>
           <Link to="/dashboard">Dashboard</Link>
           <Link to="/add_place">Add A Place</Link>
           <Link to="/privacy">Privacy Policy</Link>
