feffe9f3ad3ff8fe41227e368a6b67d6eafded53
Heather Conley <hconley6695@gmail.com>
2016-04-28 10:29:32 -0400

    adjusted dashboard and created mapview

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Link, hashHistory } from 'react-router';
-
+import { agax } from 'jquery';
 import cookie from 'js-cookie';
 
 // import FourSquare from './foursquare';
@@ -20,17 +20,42 @@ export default class Dashboard extends Component {
           hashHistory.push('/');
      }
 
+//PLACEHOLDER STATIC MAP
+
+// componentWillMount() {
+//   var map;
+//   function initMap() {
+//     let map = new google.maps.Map(document.getElementID('map'), {
+//       center: {lat: 33.751894, lng: -84.391327},
+//       zoom: 10
+//     })
+//   }
+// }
+
+//START OF COMPONENT WILL MOUNT FUNCTION
   // componentWillMount() {
   //    this.setState({locationStatus: 'Pinpointing your location..'});
 
-  //    // run the code that pinpoints the location
-  //         // then (in the callback)
-  //         // this.setState({locationStatus: 'Finding spots near you.'});
-  //         // ajax -> ask the backend for spots based on location
-  //              // then
-  //              // show them the spots
-  //              // this.setState({locationStatus: 'Found X spots near you.'});
-  // }
+  //    if ("geolocation" in navigator) {
+  //       navigator.geolocation.getCurrentPosition(function(position)  {
+  //         this.setState({locationStatus: 'Finding spots near you.'});
+
+  //         ajax('')
+
+  //       })
+  //    }
+
+  //   }   
+
+
+    // this.setState({locationStatus: 'Pinpointing your location..'});     // run the code that pinpoints the location
+     //      then (in the callback)
+     //      this.setState({locationStatus: 'Finding spots near you.'});
+     //      ajax -> ask the backend for spots based on location
+     //           then
+     //           show them the spots
+     //           this.setState({locationStatus: 'Found X spots near you.'});
+
 
   
   render() {
--- src/js/mapview.js
+++ src/js/mapview.js
@@ -1,11 +1,24 @@
 import React, {Component} from 'react';
+import $ from 'jquery';
 
 export default class MapView extends Component {
+
+	componentWillMount() {
+	  var map;
+	  function initMap() {
+	    let map = new google.maps.Map(document.getElementID('map'), {
+	      center: {lat: 33.751894, lng: -84.391327},
+	      zoom: 10
+	    })
+	  }
+	}
+
 
 	render() {
 		return (
 			<div className="mapview">
 				<span> THIS IS THE MAP VIEW OF WHERE THE PERSON IS STANDING </span>
+				<div id="map"></div>
 			</div>
 		)
 	}
