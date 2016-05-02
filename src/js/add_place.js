e97122bf4f512233ee32e580ed4766486faa2a30
Mallerie Shirley <mallerie@gmail.com>
2016-05-02 13:18:50 -0400

    updated add place component

--- src/js/add_place.js
+++ src/js/add_place.js
@@ -4,6 +4,8 @@ import SSF from 'react-simple-serial-form';
 import DropZone from 'react-dropzone';
 import { ajax, ajaxSetup } from 'jquery';
 
+import cookie from 'js-cookie';
+
 
 export default class AddPlace extends Component {
 
@@ -16,23 +18,35 @@ export default class AddPlace extends Component {
     dataHandler(data) {
 
     let newPlace = new FormData();
-    console.log(data);
+    // console.log(data);
     
-    newPlace.append('place', data.place);
-    newPlace.append('address', data.address);
-    newPlace.append('feed_area_description', data.feed_area_description);
-    newPlace.append('clean_rating', data.clean_rating);
-    newPlace.append('private_rating', data.private_ratinnewPlaceg);
-    newPlace.append('comments', data.comments);
+    newPlace.append('name', data.name);
+    newPlace.append('street', data.street);
+    newPlace.append('city', data.city);
+    newPlace.append('state', data.state);
+    newPlace.append('zipcode', data.zipcode);
+    newPlace.append('description', data.description);
+    newPlace.append('lat', data.lat);
+    newPlace.append('long', data.long);
 
     ajax({
-      url:'https://mighty-spire-68004.herokuapp.com/XXXXXXX',
+      url:'https://mighty-spire-68004.herokuapp.com/places',
       type: 'POST',
-      data: newPlace,
+      data: data,
       cached: false,
       dataType: 'json',   
-    }).then (hashHistory.push('/dashboard'))
-  
+    }).then ((response) => {
+
+        console.log('response', response)
+
+        ajaxSetup({
+          headers: {
+            'Auth-Token': response.user.auth_token 
+          }
+        })
+        cookie.set('currentUser', response.user, {expires: 3});
+        hashHistory.push('/dashboard')
+      })
     }
 
     render() {
@@ -44,30 +58,41 @@ export default class AddPlace extends Component {
           <SSF onData={::this.dataHandler}>
             <div className="add-place-form">
               <h4>Name of Place</h4>
-              <input type="text" name="place" placeholder="What would you call this place?"/>
+              <input type="text" name="name" placeholder="What would you call this place?"/>
             </div>
             <div className="add-place-form">
-              <h4>Address</h4>
-              <input type="text" name="address" placeholder="Type the Address Here"/>
+              <h4>Building Number & Street Name (add NE, SW, etc)</h4>
+              <input type="text" name="street" placeholder="Type the Address Here"/>
+            </div>
+            <div className="add-place-form">
+              <h4>City</h4>
+              <input type="text" name="city" placeholder="Type the Address Here"/>
+            </div>
+            <div className="add-place-form">
+              <h4>State</h4>
+              <input type="text" name="state" placeholder="Type the Address Here"/>
+            </div>
+            <div className="add-place-form">
+              <h4>Zip Code</h4>
+              <input type="text" name="zipcode" placeholder="Type the Address Here"/>
             </div>
             <div className="add-place-form">
               <h4>Describe the Area</h4>
-              <input type="text" name="feed_area_description" placeholder="First floor...etc"/>
+              <input type="text" name="description" placeholder="First floor...etc"/>
             </div>
-            <div className="add-place-form">
+            {/*<div className="add-place-form">
             <h4>Cleanliness Rating</h4>
-           {/*} <input type="number" name="clean_rating" placeholder="Type a Number between 1-5"/>*/}
+           
               <ul className="radio-button">
-                <li><label><input type="radio" className="input-circle" name="clean" value="1"/>Poor</label></li>
-                <li><label><input type="radio" className="input-circle" name="clean" value="2"/>Fair</label></li>
-                <li><label><input type="radio" className="input-circle" name="clean" value="3"/>Average</label></li>
-                <li><label><input type="radio" className="input-circle" name="clean" value="4"/>Good</label></li>
-                <li><label><input type="radio" className="input-circle" name="clean" value="5"/>Excellent</label></li>
+                <li><label><input type="radio" className="input-circle" name="cleanliness" value="1"/>Poor</label></li>
+                <li><label><input type="radio" className="input-circle" name="cleanliness" value="2"/>Fair</label></li>
+                <li><label><input type="radio" className="input-circle" name="cleanliness" value="3"/>Average</label></li>
+                <li><label><input type="radio" className="input-circle" name="cleanliness" value="4"/>Good</label></li>
+                <li><label><input type="radio" className="input-circle" name="cleanliness" value="5"/>Excellent</label></li>
               </ul>
             </div>
             <div className="add-place-form">
             <h4>Privacy Rating</h4>
-            {/*}  <input type="number" name="private_rating" placeholder="Type a Number between 1-5"/>*/}
               <ul className="radio-button">
                 <li><label><input type="radio" className="input-circle" name="privacy" value="1"/>Poor</label></li>
                 <li><label><input type="radio" className="input-circle" name="privacy" value="2"/>Fair</label></li>
@@ -79,7 +104,7 @@ export default class AddPlace extends Component {
             <div className="add-place-form">
               <h4>Comments</h4>
               <textarea id="comments" name="comments"/>
-            </div>
+            </div>*/}
             <div className="add-btn">
               <button id="add-place-btn">Submit</button>
             </div> 
@@ -89,3 +114,9 @@ export default class AddPlace extends Component {
         )
   }
 }
+
+// newPlace.append('cleanliness', data.cleanliness);
+//     newPlace.append('privacy', data.privacy);
+
+{/*} <input type="number" name="clean_rating" placeholder="Type a Number between 1-5"/>*/}
+{/*}  <input type="number" name="private_rating" placeholder="Type a Number between 1-5"/>*/}
