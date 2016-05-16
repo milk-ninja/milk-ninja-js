315c49c5acd3062e3519d20b965047c86dfa9f61
Heather Conley <hconley6695@gmail.com>
2016-05-15 21:15:51 -0400

    cleaned up commented out code

--- src/js/list_view.js
+++ src/js/list_view.js
@@ -18,32 +18,6 @@ export default class ListView extends Component {
 
   componentWillMount() {
 
-//     let { place } = this.props.params;
-// console.log('place', place);
-//     ajax({
-//       url:`https://mighty-spire-68004.herokuapp.com/places/find_nearby`,
-//       type: 'GET',
-//       data: {
-//         lat: '00.00',
-//         long: '00.00'
-//       },
-//       cached: false,
-//       dataType: 'json',   
-//     }).then((loadedData) => {
-
-//         console.log('loadedData', loadedData)
-
-//         // ajaxSetup({
-//         //   headers: {
-//         //     'Auth-Token': response.user.auth_token 
-//         //   }
-//         // })
-//         // this.setState({places: loadedData});
-//         // cookie.set('currentUser', response.user, {expires: 3});
-//         // hashHistory.push('/places')
-
-//       })
-
       getNearbyPlaces(data => {
         this.setState({places: data.places});
         // console.log(places);
